fb386f2b604de4801a1bf4e0de8af9aba041d005
Fabian Lukas Grubmüller <fabian.grubmueller@gmail.com>
2026-06-10 16:03:28 +0200

    Formatting

--- flake.nix
+++ flake.nix
@@ -9,21 +9,25 @@
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
 
-  in {
-    devShells.${system} = {
-      default = pkgs.mkShell {
-        packages = with pkgs; [
-          (agda.withPackages (apkgs: with apkgs; [
-            (cubical.overrideAttrs {
-              src = pkgs.fetchFromGitHub {
-                owner = "agda";
-                repo = "cubical";
-                rev = "d4a2af62de40a6ca9a0b51981e41f804d879a1b9";
-                hash = "sha256-eNslweY02wanasdsIVw3icuyQlKx5U6rbMKG5NTHFTY=";
-              };})
-          ]))
-        ];
+    in
+    {
+      devShells.${system} = {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            (agda.withPackages (
+              apkgs: with apkgs; [
+                (cubical.overrideAttrs {
+                  src = pkgs.fetchFromGitHub {
+                    owner = "agda";
+                    repo = "cubical";
+                    rev = "d4a2af62de40a6ca9a0b51981e41f804d879a1b9";
+                    hash = "sha256-eNslweY02wanasdsIVw3icuyQlKx5U6rbMKG5NTHFTY=";
+                  };
+                })
+              ]
+            ))
+          ];
+        };
       };
-    };
     };
 }

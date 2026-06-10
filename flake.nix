0415e0a3bdd7fe4195413653b29712fd8520381d
Élise Souche <elise@souche.one>
2026-06-10 15:41:37 +0200

    follow master version of agda/cubical

--- flake.nix
+++ flake.nix
@@ -12,7 +12,13 @@
       default = pkgs.mkShell {
         packages = with pkgs; [
           (agda.withPackages (apkgs: with apkgs; [
-            cubical
+            (cubical.overrideAttrs {
+              src = pkgs.fetchFromGitHub {
+                owner = "agda";
+                repo = "cubical";
+                rev = "d4a2af62de40a6ca9a0b51981e41f804d879a1b9";
+                hash = "sha256-eNslweY02wanasdsIVw3icuyQlKx5U6rbMKG5NTHFTY=";
+              };})
           ]))
         ];
       };

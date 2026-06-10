5b263dce9a315c24b10f82cd9f51d5ae080af257
Fabian Lukas Grubmüller <fabian.grubmueller@gmail.com>
2026-06-10 15:09:26 +0200

    formatting

--- flake.nix
+++ flake.nix
@@ -3,9 +3,11 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = {nixpkgs, ...}: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
 
   in {
     devShells.${system} = {
@@ -23,5 +25,5 @@
         ];
       };
     };
-  };
+    };
 }

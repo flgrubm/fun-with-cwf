4c9d6a31f934d9a555f7b6ea8de2fc03b801d63a
Fabian Lukas Grubmüller <fabian.grubmueller@gmail.com>
2026-06-10 15:06:51 +0200

    Add nix boilerplate

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+
+  in {
+    devShells.${system} = {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          (agda.withPackages (apkgs: with apkgs; [
+            cubical
+          ]))
+        ];
+      };
+    };
+  };
+}
